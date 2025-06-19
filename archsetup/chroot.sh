6b88ac46e1710839a5644b905e89ecabfa3b79e5
zedonix <zedonix@proton.me>
2025-06-19 20:47:29 +0530

    confused between min and full

--- archsetup/chroot.sh
+++ archsetup/chroot.sh
@@ -91,7 +91,7 @@ reflector --country 'India' --latest 10 --age 24 --sort rate --save /etc/pacman.
 systemctl enable reflector.timer
 
 # Copy config and dotfiles as the user
-if [[ "$second" == "min" ]]; then
+if [[ "$second" == "full" ]]; then
     su - "$username" -c '
         xdg-user-dirs-update
         mkdir -p ~/Pictures/Screenshots ~/.config ~/.local/state/bash
