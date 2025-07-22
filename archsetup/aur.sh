26a95dc6b2033475b2fd33225d439e01eda78b17
zedonarch <zedonix@proton.me>
2025-07-22 10:38:38 +0530

    less; -a for flatpak;

--- archsetup/aur.sh
+++ archsetup/aur.sh
@@ -22,7 +22,7 @@ done
 
 for pkg in "${aur_pkgs[@]}"; do
   cd "$aur_dir/$pkg" || continue
-  nvim PKGBUILD
+  less PKGBUILD
   read -rp "Build and install '$pkg'? (y/n): " reply
   if [[ -z $reply || $reply =~ ^[Yy]$ ]]; then
     makepkg -si --noconfirm --needed
@@ -33,11 +33,11 @@ done
 
 kvantummanager
 
-flatpak install org.gtk.Gtk3theme.Adwaita-dark
+flatpak install -y org.gtk.Gtk3theme.Adwaita-dark
 flatpak override --user --env=GTK_THEME=Adwaita-dark --env=QT_STYLE_OVERRIDE=Adwaita-Dark
-flatpak install flathub org.gimp.GIMP
-flatpak install flathub com.github.wwmm.easyeffects
-flatpak install flathub com.github.d4nj1.tlpui
+flatpak install -y flathub org.gimp.GIMP
+flatpak install -y flathub com.github.wwmm.easyeffects
+flatpak install -y flathub com.github.d4nj1.tlpui
 
 #ollama pull gemma3:1b
 #ollama pull codellama:7b-instruct
