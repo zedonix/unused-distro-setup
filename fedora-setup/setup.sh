914b3b9d5f97c4c0e4e88c4479784fceb8455db2
zedonarch <zedonix@proton.me>
2025-07-28 15:13:35 +0530

    grub config; file path;

--- fedora-setup/setup.sh
+++ fedora-setup/setup.sh
@@ -62,9 +62,9 @@ gunzip -c eza_x86_64-unknown-linux-gnu.tar.gz | cpio -idmv
 sudo mv eza /usr/local/bin/
 sudo chmod +x /usr/local/bin/eza
 
-xargs sudo dnf install -y <pkglist.txt
+xargs sudo dnf install -y <~/fedora_setup/pkglist.txt
 sudo dracut --force
-sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # User setup
 if [[ "$hardware" == "hardware" ]]; then
