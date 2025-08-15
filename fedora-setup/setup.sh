f827dc1e83a51ab1264c35affcd6bd058b09cf83
zedonarch <zedonix@proton.me>
2025-08-15 17:42:19 +0530

    yo

--- fedora-setup/setup.sh
+++ fedora-setup/setup.sh
@@ -11,7 +11,7 @@ cd "$SCRIPT_DIR"
 
 # Variable set
 username=piyush
-CHROOT="fedora-43-x86_64"
+CHROOT="fedora-42-x86_64"
 
 # Which type of install?
 # First choice: vm or hardware
@@ -75,7 +75,7 @@ sudo dnf upgrade --refresh
 ## Adding repos
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
+sudo dnf install -y dnf-plugins-core
 if ! copr-cli build maximizerr/SwayAura --chroot "$CHROOT"; then
     echo "ERROR: Chroot $CHROOT not found in maximizerr/SwayAura."
     echo "Please choose one from the following:"
