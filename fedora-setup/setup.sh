11b377dd142788e26f8d826f58ddaeefcabfc043
zedonarch <zedonix@proton.me>
2025-08-15 17:49:21 +0530

    yo

--- fedora-setup/setup.sh
+++ fedora-setup/setup.sh
@@ -75,14 +75,16 @@ sudo dnf upgrade --refresh
 ## Adding repos
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y dnf-plugins-core
+sudo dnf install -y copr-cli
 if ! copr-cli build maximizerr/SwayAura --chroot "$CHROOT"; then
     echo "ERROR: Chroot $CHROOT not found in maximizerr/SwayAura."
     echo "Please choose one from the following:"
     copr-cli list-chroots maximizerr/SwayAura
     exit 1
 fi
-sudo dnf -y copr enable solopasha/hyprland maximizerr/SwayAura
+sudo dnf copr enable solopasha/hyprland:fedora-42-x86_64
+sudo dnf copr enable maximizerr/SwayAura:fedora-42-x86_64
+sudo dnf makecache
 
 # pacstrap of fedora
 xargs sudo dnf install -y <pkglist.txt
