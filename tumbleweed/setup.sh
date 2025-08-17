6d9deab8d477fc62257d79e6561cf3b34fba6198
zedonarch <zedonix@proton.me>
2025-08-17 12:54:49 +0530

    yo

--- tumbleweed/setup.sh
+++ tumbleweed/setup.sh
@@ -62,7 +62,7 @@ fi
 
 sudo zypper ar -f obs://home:amirsina Tumbleweed-Persepolis
 sudo zypper ar -f obs://home:iDesmI ananicy-cpp
-sudo zypper ar -cfp 90 http://packman.links2linux.de/mirrors/openSUSE_Tumbleweed/ packman
+sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 sudo zypper ar -cfp 98 http://codecs.opensuse.org/openh264/openSUSE_Tumbleweed/ openh264
 sudo zypper ref
 sudo zypper dup --from packman --allow-vendor-change
