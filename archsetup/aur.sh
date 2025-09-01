653fecd33fd70c9d0d9d7f0a56e20e18c0fd875b
zedonarch <zedonix@proton.me>
2025-09-01 12:25:09 +0530

    no anancity

--- archsetup/aur.sh
+++ archsetup/aur.sh
@@ -35,8 +35,6 @@ done
 
 kvantummanager
 
-sudo systemctl enable ananicy-cpp
-
 flatpak install -y org.gtk.Gtk3theme.Adwaita-dark
 flatpak override --user --env=GTK_THEME=Adwaita-dark --env=QT_STYLE_OVERRIDE=Adwaita-Dark
 # flatpak install -y flathub org.gimp.GIMP
