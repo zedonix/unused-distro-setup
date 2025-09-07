350041e1bb345fdbea959de071f343080c517986
zedonarch <zedonix@proton.me>
2025-09-07 18:41:12 +0530

    same; now next commit fixes anacicy (I hope so)

--- archsetup/chroot.sh
+++ archsetup/chroot.sh
@@ -269,6 +269,15 @@ if [[ "$howMuch" == "max" ]]; then
   cp -r "$THEME_SRC/themes/Gruvbox-Material-Dark" "$THEME_DEST/themes"
   cp -r "$THEME_SRC/icons/Gruvbox-Material-Dark" "$THEME_DEST/icons"
 
+  # Anancy-cpp rules
+  git clone --depth=1 https://github.com/RogueScholar/ananicy.git
+  git clone --depth=1 https://github.com/CachyOS/ananicy-rules.git
+  mkdir -p /etc/ananicy.d/roguescholar /etc/ananicy.d/zz-cachyos
+  cp -r ananicy/ananicy.d/* /etc/ananicy.d/roguescholar/
+  cp -r ananicy-rules/00-default/* /etc/ananicy.d/zz-cachyos/
+  cp -r ananicy-rules/00-types.types /etc/ananicy.d/zz-cachyos/
+  cp -r ananicy-rules/00-cgroups.cgroups /etc/ananicy.d/zz-cachyos/
+
   # Firefox policy
   mkdir -p /etc/firefox/policies
   ln -sf "/home/$username/Documents/projects/default/dotfiles/policies.json" /etc/firefox/policies/policies.json
@@ -331,7 +340,7 @@ mkdir -p /etc/systemd/zram-generator.conf.d
 # rfkill unblock bluetooth
 # modprobe btusb || true
 if [[ "$howMuch" == "max" ]]; then
-  systemctl enable ly cronie sshd reflector.timer
+  systemctl enable ananicy-cpp ly cronie sshd reflector.timer
   if [[ "$hardware" == "hardware" ]]; then
     systemctl enable fstrim.timer acpid libvirtd.socket cups ipp-usb docker.socket
   fi
