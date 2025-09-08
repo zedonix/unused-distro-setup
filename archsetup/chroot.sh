72ec3f502bf7eec9f1dc148ceed0ecc896958612
zedonarch <zedonix@proton.me>
2025-09-08 09:22:30 +0530

    sowwy, lemme try again

--- archsetup/chroot.sh
+++ archsetup/chroot.sh
@@ -277,22 +277,11 @@ if [[ "$howMuch" == "max" ]]; then
   cp -r ananicy-rules/00-default/* /etc/ananicy.d/zz-cachyos/
   cp -r ananicy-rules/00-types.types /etc/ananicy.d/zz-cachyos/
   cp -r ananicy-rules/00-cgroups.cgroups /etc/ananicy.d/zz-cachyos/
-#   sudo tee /etc/ananicy.d/00-system-protect.rules >/dev/null <<'EOF'
-# [polkitd]
-# nice = 0
-# ionice = 0
-# sched = keep
-#
-# [dbus-daemon]
-# nice = 0
-# ionice = 0
-# sched = keep
-#
-# [systemd-logind]
-# nice = 0
-# ionice = 0
-# sched = keep
-# EOF
+  tee /etc/ananicy.d/99-system-protect.rules >/dev/null <<'EOF'
+{ "name": "polkitd", "nice": 0, "ioclass": "best-effort", "sched": "keep" }
+{ "name": "dbus-daemon", "nice": 0, "ioclass": "best-effort", "sched": "keep" }
+{ "name": "systemd-logind", "nice": 0, "ioclass": "best-effort", "sched": "keep" }
+EOF
 
   # Firefox policy
   mkdir -p /etc/firefox/policies
