6315799b2d730f5afaef8b1153a063bc56d78d75
zedonarch <zedonix@proton.me>
2025-09-13 09:03:56 +0530

    automated commit

--- tumbleweed/install.sh
+++ tumbleweed/install.sh
@@ -64,12 +64,15 @@ fi
 # sudo zypper ar -f http://codecs.opensuse.org/openh264/openSUSE_Tumbleweed repo-openh264
 sudo zypper --gpg-auto-import-keys ar -cf obs://home:iDesmI ananicy-cpp
 sudo zypper --gpg-auto-import-keys ar -cfp 90 http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
+sudo zypper --gpg-auto-import-keys refresh
 sudo zypper rr repo-openh264
 sudo zypper ref -f
 sudo zypper dup --from packman --allow-vendor-change
 
 xargs -a pkglist.txt sudo zypper install -y
-corepack enable
+export PATH="$HOME/.local/bin:$PATH"
+corepack enable --install-directory ~/.local/bin
+corepack prepare pnpm@latest --activate
 pnpm add -g stylelint stylelint-config-standard
 pipx ensurepath
 pipx install thefuck
