dca02c10f0eaa9bc96074e6ab7b6b31a3d023ed3
zedonarch <zedonix@proton.me>
2025-09-13 17:41:03 +0530

    automated commit

--- tumbleweed/install.sh
+++ tumbleweed/install.sh
@@ -97,7 +97,7 @@ cd "$(mktemp -d)"
 cd ..
 git clone https://codeberg.org/fairyglade/ly.git
 cd ly
-zig build
+zig build -Dinit_system=systemd
 sudo zig build installexe -Dinit_system=systemd
 # Iosevka
 mkdir -p ~/.local/share/fonts/iosevka
