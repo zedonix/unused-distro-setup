24308cb705f618bff67ec1d7cd1e632d196e260a
zedonarch <zedonix@proton.me>
2025-09-13 19:59:41 +0530

    it was a path prob

--- tumbleweed/setup.sh
+++ tumbleweed/setup.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_DIR"
 
 username=piyush
-
+export PATH="/sbin:/usr/sbin:$PATH"
 cd "$(mktemp -d)"
 # # wikiman
 # RPM_URL=$(curl -s https://api.github.com/repos/filiparag/wikiman/releases/latest |
